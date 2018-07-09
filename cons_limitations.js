af47a0e2fd41e46b01a9d9eb2d31b7eaf7a24009
carlogilmar <carlogilmar12@gmail.com>
2018-07-09 02:45:02 -0500

    Adding script example of const limitations

---
+++ cons_limitations.js
@@ -0,0 +1,11 @@
+const max = 200;
+const ok = true;
+const nothing = undefined;
+const nope = null;
+const sam = { first: 'Sam', age: 2 };
+//max = 200; //Not allowed
+//ok = true;  //Not allowed
+//nothing = undefined; //Not allowed
+//nope = null;  //Not allowed
+//sam = { first: 'Sam', age: 2 }; //Not allowed
+sam.age = 3;
