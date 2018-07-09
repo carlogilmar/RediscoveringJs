911bd42d61315e690714f1357bc1c2061fc71e7f
carlogilmar <carlogilmar12@gmail.com>
2018-07-09 02:15:36 -0500

    Adding messy js example as everything js

---
+++ messy-eslint.js
@@ -0,0 +1,21 @@
+const sample = function(number) {
+
+	factor = 4;
+
+	if(number == 2) {
+		return
+    number * factor;
+	}
+
+	return number * 10;
+};
+
+console.log(sample(2));
+
+/*
+	Js Lint code
+npm install -g eslint
+
+Create the configuration
+eslint --init
+*/
