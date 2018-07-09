b2b6bdab1f311aa5302adffc3c3595969be4984b
carlogilmar <carlogilmar12@gmail.com>
2018-07-09 02:09:45 -0500

    Init commit

---
+++ strict.js
@@ -0,0 +1,9 @@
+//BROKEN CODE
+'use strict';
+const oops = function() {
+haha = 2;
+  console.log(haha);
+};
+oops();
+console.log(haha);
+// this should be broken by the strict because it doesn't allow undeclared variables
